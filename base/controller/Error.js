22af462652e0c5b4926c49de5dec29a422b24acf
Orlando Leite <orlleite@gmail.com>
2018-10-29 14:42:58 -0400

    Fixed router

--- base/controller/Error.js
+++ base/controller/Error.js
@@ -9,22 +9,15 @@ module.exports = class Error extends mvc.BaseController
 {
     error404( ctx )
     {
-        ctx.set('Access-Control-Allow-Credentials', 'true');
-        ctx.set('Access-Control-Allow-Origin', process.env.CMS_DOMAIN);
-        ctx.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-        
         ctx.response.status = 404;
-        ctx.body = { status: false, error: 404 };
+        ctx.body = 'Error 404'+ 
+            ( process.env.ENV == "development" ? "\n" + ctx.serverError.message : "" );;
     }
 
     error( ctx )
     {
-        ctx.set('Access-Control-Allow-Credentials', 'true');
-        ctx.set('Access-Control-Allow-Origin', process.env.CMS_DOMAIN);
-        ctx.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-
         ctx.response.status = ctx.serverError.status || 500;
-        ctx.body = "Error 500" + 
+        ctx.body = "Error " + ctx.response.status + 
             ( process.env.ENV == "development" ? "\n" + ctx.serverError.message : "" );
 
         ctx.app.emit('error', ctx.serverError, ctx);
